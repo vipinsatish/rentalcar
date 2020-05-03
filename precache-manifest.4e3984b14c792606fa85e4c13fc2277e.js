6ce7896d39c0211fc8f7104d2b942ef0a74c25a4
vipinsatish <vipinsatish@gmail.com>
2020-05-03 07:09:41 +0530

    Updates

---
+++ precache-manifest.4e3984b14c792606fa85e4c13fc2277e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "916f155caff61b4761ab",
+    "url": "/rentalcar/static/css/main.4cb95b6b.chunk.css"
+  },
+  {
+    "revision": "916f155caff61b4761ab",
+    "url": "/rentalcar/static/js/main.916f155c.chunk.js"
+  },
+  {
+    "revision": "15d22dc8f46545904930",
+    "url": "/rentalcar/static/js/runtime~main.15d22dc8.js"
+  },
+  {
+    "revision": "fd1b4b6055f4e97d975c",
+    "url": "/rentalcar/static/js/2.fd1b4b60.chunk.js"
+  },
+  {
+    "revision": "b2d48329f4fd9d14dd28771b496a8425",
+    "url": "/rentalcar/index.html"
+  }
+];
